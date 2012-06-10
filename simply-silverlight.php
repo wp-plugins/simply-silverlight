76efdfae557591a5b4666d4b4c10ab3bb5224af5
david_wp <david_wp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-10 03:19:57 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/simply-silverlight/tags/1.0.1@555521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simply-silverlight.php
+++ simply-silverlight.php
@@ -3,14 +3,14 @@
 	Plugin Name: Simply Silverlight
 	Plugin URI: http://www.digitalwindfire.com/software/simply-silverlight
 	Description: Bring the power of Microsoft's visually rich Silverlight technology to your WordPress site today! Simply Silverlight makes deploying Silverlight .xap files to your WordPress blog's posts and pages incredibly easy and intuitive. Don't hesitate, click Activate now!
-	Version: 1.0
+	Version: 1.0.1
 	Author: David Wright
 	Author URI: http://www.digitalwindfire.com
 	License: GPL2
 */
 
 /*
-	Copyright 2011  David Wright  (email : davidwright@digitalwindfire.com)
+	Copyright 2011-2012  David Wright  (email : davidwright@digitalwindfire.com)
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -26,6 +26,51 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+// get current settings from database
+$options = get_option('simply-silverlight-settings');
+
+// verify settings exist in database and none are missing
+if (!is_array($options)) {
+	// install default settings	
+	update_option('simply-silverlight-settings', simply_silverlight_default_settings());
+} else {
+	// check for missing settings
+	if (!array_key_exists('pluginversion', $options))    {$options['pluginversion'] = '1.0.1';}
+	if (!array_key_exists('path', $options))             {$options['path'] = '/ClientBin/';}
+	if (!array_key_exists('securepath', $options))       {$options['securepath'] = dirname(__FILE__).DIRECTORY_SEPARATOR.'secure'.DIRECTORY_SEPARATOR;}
+	if (!array_key_exists('accesscontrol', $options))    {$options['accesscontrol'] = '';}
+	if (!array_key_exists('width', $options))            {$options['width'] = '400';}
+	if (!array_key_exists('height', $options))           {$options['height'] = '300';}
+	if (!array_key_exists('display', $options))          {$options['display'] = 'inline';}
+	if (!array_key_exists('float', $options))            {$options['float'] = 'left';}
+	if (!array_key_exists('style', $options))            {$options['style'] = '';}
+	if (!array_key_exists('background', $options))       {$options['background'] = 'white';}
+	if (!array_key_exists('version', $options))          {$options['version'] = '4.0.60531.0';}
+	if (!array_key_exists('autoupgrade', $options))      {$options['autoupgrade'] = 'true';}
+	if (!array_key_exists('preservesettings', $options)) {$options['preservesettings'] = 'true';}	
+	
+	update_option('simply-silverlight-settings', $options);
+}
+
+// get default settings
+function simply_silverlight_default_settings() {
+	$options = array('pluginversion'    => '1.0.1',
+	                 'path'             => '/ClientBin/',
+					 'securepath'       => dirname(__FILE__).DIRECTORY_SEPARATOR.'secure'.DIRECTORY_SEPARATOR,
+					 'accesscontrol'    => '',
+                     'width'            => '400',
+                     'height'           => '300',
+                     'display'          => 'inline',
+                     'float'            => 'left',
+                     'style'            => '',
+                     'background'       => 'white',
+                     'version'          => '4.0.60531.0',
+                     'autoupgrade'      => 'true',
+					 'preservesettings' => 'true');
+    
+	return $options;
+}
 
 // include file containing html display functions
 include 'simply-silverlight-html.php';
@@ -48,6 +93,12 @@ add_action('admin_init', 'simply_silverlight_build_admin_page');
 // add shortcode handler
 add_shortcode('simply-silverlight', 'simply_silverlight_shortcode_handler');
 
+// add filter for url variable
+add_filter('query_vars', 'simply_silverlight_query_vars');
+
+// add action to process .xap request
+add_action('parse_request', 'simply_silverlight_parse_request');
+
 // activation function
 function simply_silverlight_activate() {
 	// check version compatibility
@@ -57,20 +108,6 @@ function simply_silverlight_activate() {
 	if (version_compare($wp_version, "3.0", "<"))	{
 		exit($exit_msg);
 	}
-	
-	// set default options	
-	$options = array('path'             => '/ClientBin/',
-                     'width'            => '400',
-                     'height'           => '300',
-                     'display'          => 'inline',
-                     'float'            => 'left',
-                     'style'            => '',
-                     'background'       => 'white',
-                     'version'          => '4.0.60531.0',
-                     'autoupgrade'      => 'true',
-					 'preservesettings' => 'true');
-    
-	add_option('simply-silverlight-settings', $options);
 }
 
 // deactivation function
@@ -98,10 +135,12 @@ function simply_silverlight_build_admin_page() {
 	register_setting('simply-silverlight-settings-group', 'simply-silverlight-settings', 'simply_silverlight_validate_options');
 	
 	// add main settings section
-	add_settings_section('main', '', '', 'simply_silverlight');
+	add_settings_section('main', '', 'simply_silverlight_main_section_html', 'simply_silverlight');
 	
 	// add settings fields to main section	
-	add_settings_field('simply-silverlight-settings[path]', __('Path','simply-silverlight'), 'simply_silverlight_path_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[path]'));	
+	add_settings_field('simply-silverlight-settings[path]', __('Path','simply-silverlight'), 'simply_silverlight_path_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[path]'));
+	add_settings_field('simply-silverlight-settings[securepath]', __('Secure Path','simply-silverlight'), 'simply_silverlight_securepath_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[securepath]'));
+	add_settings_field('simply-silverlight-settings[accesscontrol]', __('Access Control','simply-silverlight'), 'simply_silverlight_accesscontrol_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[accesscontrol]'));
 	add_settings_field('simply-silverlight-settings[width]', __('Width','simply-silverlight'), 'simply_silverlight_width_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[width]'));
 	add_settings_field('simply-silverlight-settings[height]', __('Height','simply-silverlight'), 'simply_silverlight_height_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[height]'));
 	add_settings_field('simply-silverlight-settings[background]', __('Background','simply-silverlight'), 'simply_silverlight_background_setting_html', 'simply_silverlight', 'main', array('label_for' => 'simply-silverlight-settings[background]'));
@@ -115,13 +154,22 @@ function simply_silverlight_build_admin_page() {
 
 // validate options
 function simply_silverlight_validate_options($in) {
-	// get current settings from database
-	$options = get_option('simply-silverlight-settings');
+    // reset plugin default settings
+    if (isset($_POST["simply-silverlight-reset"])) {
+      $_REQUEST["_wp_http_referer"] = add_query_arg("reset", "true", $_REQUEST["_wp_http_referer"]);
+      return simply_silverlight_default_settings();
+    }	
 	
-	// copy only known settings to a new array; path, width and height will be validated
+	// get current settings from database
+	$options = get_option('simply-silverlight-settings');	
+	
+	// copy only known settings to a new array; path, securepath, width and height will be validated
+	$out['pluginversion']    = $options['pluginversion'];
 	$out['path']             = $options['path'];
+	$out['securepath']       = $options['securepath'];	
 	$out['width']            = $options['width'];
 	$out['height']           = $options['height'];
+	$out['accesscontrol']    = $in['accesscontrol'];
 	$out['background']       = $in['background'];
 	$out['display']          = $in['display'];
 	$out['float']            = $in['float'];
@@ -145,7 +193,32 @@ function simply_silverlight_validate_options($in) {
 	} else {
 		$out['path'] = $in['path'];
 	}
-
+	
+	// warn if secure path doesn't exist
+	if (strlen($in['securepath']) > 0) {
+		if (!is_dir($in['securepath'])) {
+			add_settings_error('Secure Path', 'securepath-invalid', __('Secure path does not exist.', 'simply-silverlight'), 'error');
+			
+			// allow the path to change even if it doesn't exist
+			$options['securepath'] = $in['securepath'];
+			
+			return $options;
+		} else {
+			// resolve relative path to absolute
+			$in['securepath'] = realpath($in['securepath']);
+			
+			// ensure the secure path ends with correct directory separator
+			if (substr($in['securepath'], -1) != DIRECTORY_SEPARATOR) {
+				$out['securepath'] = $in['securepath'].DIRECTORY_SEPARATOR;
+			} else {
+				$out['securepath'] = $in['securepath'];
+			}
+		}
+	} else {
+		// allow deletion of the secure path
+		$out['securepath'] = $in['securepath'];
+	}
+	
 	// width
 	if (!preg_match('/^[0-9]{1,4}$/i', $in['width'])) {
 		add_settings_error('Width', 'width-invalid', __('Width must be a number not greater than 9999.', 'simply-silverlight'), 'error');
@@ -177,18 +250,21 @@ function simply_silverlight_shortcode_handler($atts) {
 	$options = get_option('simply-silverlight-settings');
 	
 	extract(shortcode_atts(array(
-		'path'        => $options['path'],
-		'width'       => $options['width'],
-		'height'      => $options['height'],
-		'display'     => $options['display'],
-		'float'       => $options['float'],
-		'style'       => $options['style'],
-		'background'  => $options['background'],
-		'version'     => $options['version'],
-		'autoupgrade' => $options['autoupgrade'],
-		'onerror'     => '',
-		'initparams'  => '',
-		'xap'         => ''		
+		'path'          => $options['path'],
+		'securepath'    => $options['securepath'],
+		'accesscontrol' => $options['accesscontrol'],
+		'width'         => $options['width'],
+		'height'        => $options['height'],
+		'display'       => $options['display'],
+		'float'         => $options['float'],
+		'style'         => $options['style'],
+		'background'    => $options['background'],
+		'version'       => $options['version'],
+		'autoupgrade'   => $options['autoupgrade'],
+		'onerror'       => '',
+		'initparams'    => '',
+		'xap'           => '',
+		'secure'        => ''		
 	), $atts));
 
 	// if initparams supplied wrap in a param element
@@ -204,8 +280,13 @@ function simply_silverlight_shortcode_handler($atts) {
 		$onerror = "<param name=\"onError\" value=\"$onerror\" />";
 	}
 
-	// ensure only a single slash exists between the $path and $xap
-	$source = rtrim($path, '/').'/'.ltrim($xap, '/');
+	// set source .xap url
+	if ($secure == 'true') {
+	    $source = get_option('siteurl').'/index.php?simply-silverlight='.trim($xap, '/');
+	} else {
+		// ensure only a single slash exists between the $path and $xap
+		$source = rtrim($path, '/').'/'.ltrim($xap, '/');
+	}
 
 	// the html for the silverlight control
 	$html = <<<EOT
@@ -225,5 +306,76 @@ function simply_silverlight_shortcode_handler($atts) {
 		</div>
 EOT;
 	return $html;
+}
+
+// process request for .xap
+function simply_silverlight_parse_request($wp) {
+    // process requests for simply-silverlight
+    if (array_key_exists('simply-silverlight', $wp->query_vars) && $wp->query_vars['simply-silverlight'] != '') {
+		// .xap file requested
+		$requested_xap = $wp->query_vars['simply-silverlight'];
+
+		// get current settings from database
+		$options = get_option('simply-silverlight-settings');
+
+		// if user isn't logged in exit function
+		if (!is_user_logged_in()) {
+			wp_die('There was a problem with the request.');
+		}
+		
+		// default authentication
+		$authenticated = true;
+		
+		// determine if requested .xap has an acl
+		if (preg_match("/$requested_xap\((.*?)\)/i", $options['accesscontrol'], $matches)) {
+			// change default authentication to false
+			$authenticated = false;
+			
+			// get acl
+			$acl = explode(',', strtolower($matches[1]));
+								
+			// current user login and roles
+			global $current_user;
+			global $user_login;
+			get_currentuserinfo();
+			$roles = $current_user->roles;			
+			
+			// check user login
+			if (in_array(strtolower($user_login), $acl)) {
+				$authenticated = true;
+			} else {
+				// if user login fails, check roles
+				if (!$authenticated) {
+					foreach($roles as $role) {
+						if (in_array(strtolower($role), $acl)) {
+							$authenticated = true;
+							break;
+						}
+					}				
+				}
+			}
+		}
+		
+		// if authenticated send the .xap
+		if ($authenticated) {
+			$filename = $options['securepath'].$requested_xap;
+	
+			if (file_exists($filename)) {
+				$len = filesize($filename);
+				header("Content-Length: $len");
+				header("Content-Type: application/x-silverlight-app");
+				header("Content-Disposition: attachment; filename=\"$requested_xap\"");
+				readfile($filename);
+			}			
+		} else {
+			wp_die('There was a problem with the request.');
+		}
+    }
+}
+
+// process url variable
+function simply_silverlight_query_vars($vars) {
+    $vars[] = 'simply-silverlight';
+    return $vars;
 }
 ?>
